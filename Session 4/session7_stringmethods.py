0c6875d873501ba43a844646d0d2fe97f66334b5
issywrigs <iwrigley.ieu2023@student.ie.edu>
2025-01-29 18:05:37 +0100

    string operations

---
+++ Session 4/session7_stringmethods.py
@@ -0,0 +1,8 @@
+s= "hello"
+print(dir(s)) #gives a list of methods
+print(help(s.capitalize))
+print(s.capitalize)
+print("HELLO".casefold())
+print("hello".center(50))
+print("hello".center(50, "*"))
+
---
+++ Session 4/session7_stringslices.py
@@ -0,0 +1,10 @@
+s="0123456789"
+print(s)
+print(s[1:2]) #includes first index, excludes last
+print(s[1:7])
+print(s[:7]) #starts from beginning
+print(s[5:]) # prints from the 6th character to the end
+print(s[::]) # prints the whole thing
+print(s[::2]) #prints every other character
+print(s[::-1]) #reverses
+
