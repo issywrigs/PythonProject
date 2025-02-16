af003ea35f66e343b1f8714c77e92569c8d355f1
issywrigs <iwrigley.ieu2023@student.ie.edu>
2025-02-16 18:41:22 +0100

    Operators

---
+++ Programming for Economists II/Session 3&4/Operators.py
@@ -0,0 +1,36 @@
+print(7+2)
+print(7-2)
+print(7*2)
+print(7/2)
+print(7//2)
+print(7%2)
+print(-7)
+print(abs(7))
+print(int(7/2))
+print(float(7))
+print(7**2)
+
+#Boolean Operators
+# == equal, != not equal, "is" gives an object identity and "is not" does the opposite
+
+#bitewise operators
+#x | y gives bitewise, x^y bitewise exclusive of x and y, x&y bitewise of x and y, x << n x shifted left by n bits
+
+#assignment operators
+# +=   x += 5   x = x + 5
+# -=  x -= 5   x = x - 5
+# *=   x *= 5   x = x * 5
+# /=   x /= 5   x = x / 5
+# %=  x %= 5    x = x % 5
+# //=   x //= 5    x = x // 5
+# **=   x **= 5    x = x ** 5
+# &=    x &= 5    x = x & 5
+# |=      x |= 5    x = x | 5
+# ^=    x ^= 5   x = x ^ 5
+# >>=    x >>= 5    x = x >> 5
+# <<=    x <<= 5    x = x << 5
+
+# To check operator type use function type()
+print(type(3 | 2.0))
+
+
