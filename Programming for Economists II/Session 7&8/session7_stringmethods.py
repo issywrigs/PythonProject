9e05ac2343d3981715305eb748a5c9a7ddf1ca83
issywrigs <iwrigley.ieu2023@student.ie.edu>
2025-02-19 11:44:42 +0100

    Parsing strings using string methods added

--- Programming for Economists II/Session 7&8/session7_stringmethods.py
+++ Programming for Economists II/Session 7&8/session7_stringmethods.py
@@ -21,4 +21,22 @@ print("hello".center(50, "*"))
 
 print("banananananananana". count("ana"))
 
+#Parsing Strings Using String Methods
+#Text that you want to extract all links from
+#link always starts with http:// and ends with a space
+s = "http://google.com and then there could be http://yahoo.com or even a website like http://bbc.co.uk"
+start = 0
+while True:
+    start = s.find("http://", start)
+    if start == -1:
+        break
+    end = s.find(" ",start)
+    if end == -1:
+        print(s[start:])
+        break
+    print(s[start:end])
+    start = end
 
+#find() method finds the first occurrence of a string inside another.
+# The second parameter indicates the position where to start.
+# If it can not find, will return -1.
