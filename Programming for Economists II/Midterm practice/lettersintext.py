ef1508c70e6a385f630d66d85d3ecd055b78180e
issywrigs <iwrigley.ieu2023@student.ie.edu>
2025-02-19 09:53:06 +0100

    Letters in texts, 2 examples

--- Programming for Economists II/Midterm practice/lettersintext.py
+++ Programming for Economists II/Midterm practice/lettersintext.py
@@ -9,3 +9,25 @@ def number_times(filename, letter):
 
 
 print("The letter e turns up", number_times("short.txt", "e"), "times")
+
+#How many times do the 2 most common letters appear i the text file?
+def most_common(filename):
+    fd = open(filename)
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    dic = {}
+    for line in fd:
+        for letter in line:
+            if letter in letters:
+                if letter in dic:
+                    dic[letter] += 1
+                else:
+                    dic[letter] = 1
+
+    list_values = list(dic.values())
+
+    list_values.sort(reverse=True)
+    fd.close()
+    print("the two most common letters appear", list_values[0] + list_values[1], "times")
+
+
+most_common("short.txt")
