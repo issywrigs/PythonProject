afb6e682e657e08c81785fc18ee354e47da39ec0
issywrigs <iwrigley.ieu2023@student.ie.edu>
2025-02-19 12:52:53 +0100

    File write extra examples added

--- Programming for Economists II/Session 9&10/file.examples_write.py
+++ Programming for Economists II/Session 9&10/file.examples_write.py
@@ -1,3 +1,8 @@
+#Suppose we want to write data to a file as well.
+#In this case we need to open the file for writing.
+#The modes are “w” – write and “a” – append.
+#“w” – will start with an empty file no matter what, “a” will append to the end if the file already exists.
+
 #lets create a virtual diary
 with open("diary.txt", "w") as fp:
     while True:
@@ -6,4 +11,15 @@ with open("diary.txt", "w") as fp:
             break
         fp.write(f"{text}/n") # this is the same as below line
         # fp.write(text + "/n")
-        
+
+#Once the file has been opened, we use the write() method.
+#Unlike print(), write() will not append a newline character at the end
+
+fp = open("text.txt", "a")
+print(fp)
+line = input("Give me some text, don't be shy: ")
+fp.write(line)
+fp.write("\n")
+fp.close()
+
+#Running this program repeatedly will keep appending to the file
