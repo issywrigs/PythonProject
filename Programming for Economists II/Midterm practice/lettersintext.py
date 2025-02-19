dc43ff39626ea77ea1de220648e02732d593da14
issywrigs <iwrigley.ieu2023@student.ie.edu>
2025-02-19 09:51:06 +0100

    Letters in texts

---
+++ Programming for Economists II/Midterm practice/lettersintext.py
@@ -0,0 +1,11 @@
+#How many times does the letter e appear in the short text?
+def number_times(filename, letter):
+    fd = open(filename)
+    times = 0
+    for line in fd:
+        times = times + line.count(letter)
+    fd.close()
+    return times
+
+
+print("The letter e turns up", number_times("short.txt", "e"), "times")
